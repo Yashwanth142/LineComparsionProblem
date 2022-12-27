03a08ec6b25e2319a6836dc99664653a116f2e5d
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2022-12-27 16:43:55 +0530

    Line Equality added

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
-            LineLength.cal_len();
+
+            LineEqual.equal();
         }
     }
 }
